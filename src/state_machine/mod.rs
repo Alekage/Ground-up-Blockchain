285f43ce013cf1c2431e2c8e6dac0677063a6a7b
Ogi-Git <ognjen25aleksic@gmail.com>
2024-09-01 12:43:36 +0200

    First commit

---
+++ src/state_machine/mod.rs
@@ -0,0 +1,14 @@
+
+
+pub trait StateMachine {
+    type State;
+    type Transition;
+
+
+    fn next_state(starting_state: &Self::State, transition: &Self::Transition) -> Self::State;
+
+    fn human_name() -> String {
+        "Unnamed state machine".into()
+    }
+
+}
